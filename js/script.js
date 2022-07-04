0534e291f6a05fde906a00987c22130ae330d944
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-07-04 16:25:42 +0200

    Complete

--- js/script.js
+++ js/script.js
@@ -12,26 +12,28 @@ var app = new Vue(
         randomMail: null
   },
   methods : {
-    // Prende una mail random dall'API
+    
+    // Scarica una mail random dall'API
     getRandomMailFromApi() {
 
       // ciclo for per 10 volte:
       for(let i = 0; i < 10; i++) {
-
         // Chiamata API:
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
-
           // Salva la mail random in una variabile:
           this.randomMail = response.data.response;
-
           // Pusha la mail random nell'array:
           this.emailList.push(this.randomMail);
         });
       }
     }
   },
+
+  // Lancia al caricamento della pagina:
   mounted() {
+
+    // richiama la funzione che scarica una mail random dall'API:
     this.getRandomMailFromApi();
   }
 }
